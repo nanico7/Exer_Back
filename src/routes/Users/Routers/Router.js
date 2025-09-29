cb4ab9e3b56d3638a4aabfa1f8d31122e1026b31
Thiago <neckelmachadothiago@gmail.com>
2025-09-29 19:01:52 -0300

    commit

--- src/routes/Users/Routers/Router.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { registrarUsuario } from "../Controllers/Controller.js";
-
-const roteadorUser = Router();
-
-roteadorUser.post("/", registrarUsuario);
-
-export default roteadorUser;
